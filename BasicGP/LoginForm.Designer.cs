3797e76a830929fd8a113069a0f8ec5ba5da01e9
ahmedi98 <ia403@student.anglia.ac.uk>
2017-10-31 13:43:33 +0000

    Adjusted login form size #5

--- BasicGP/LoginForm.Designer.cs
+++ BasicGP/LoginForm.Designer.cs
@@ -46,9 +46,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtUsername.ForeColor = System.Drawing.Color.Gray;
             this.txtUsername.Location = new System.Drawing.Point(203, 162);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(260, 22);
+            this.txtUsername.Size = new System.Drawing.Size(255, 22);
             this.txtUsername.TabIndex = 1;
             this.txtUsername.Text = "Username";
             this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
@@ -62,9 +62,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtPassword.ForeColor = System.Drawing.Color.Gray;
             this.txtPassword.Location = new System.Drawing.Point(203, 194);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(260, 22);
+            this.txtPassword.Size = new System.Drawing.Size(255, 22);
             this.txtPassword.TabIndex = 2;
             this.txtPassword.Text = "Password";
             this.txtPassword.Enter += new System.EventHandler(this.TxtPassword_Enter);
@@ -77,7 +77,7 @@
             this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnLogin.ForeColor = System.Drawing.SystemColors.ControlLight;
             this.btnLogin.Location = new System.Drawing.Point(276, 327);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(115, 55);
             this.btnLogin.TabIndex = 0;
@@ -97,9 +97,9 @@
             this.panel1.Controls.Add(this.txtUsername);
             this.panel1.ForeColor = System.Drawing.Color.Brown;
             this.panel1.Location = new System.Drawing.Point(389, 245);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(667, 463);
+            this.panel1.Size = new System.Drawing.Size(662, 551);
             this.panel1.TabIndex = 3;
             // 
             // authStatus
@@ -133,7 +133,7 @@
             this.picLogo.Image = global::BasicGP.Properties.Resources.logo1;
             this.picLogo.InitialImage = global::BasicGP.Properties.Resources.logo;
             this.picLogo.Location = new System.Drawing.Point(1252, 15);
-            this.picLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picLogo.Margin = new System.Windows.Forms.Padding(4);
             this.picLogo.Name = "picLogo";
             this.picLogo.Size = new System.Drawing.Size(169, 124);
             this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -145,12 +145,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(1437, 831);
+            this.ClientSize = new System.Drawing.Size(1432, 919);
             this.Controls.Add(this.picLogo);
             this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1458, 966);
             this.Name = "LoginForm";
