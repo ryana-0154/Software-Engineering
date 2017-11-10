5fea9ef0581e77dd2ef1513c5ccb28623ae0e07c
Ryan Alderton <aldertonr01@gmail.com>
2017-11-10 12:21:14 +0000

    made the availability form

---
+++ BasicGP/AvailabilityForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicGP
+{
+    public partial class AvailabilityForm : Form
+    {
+        public AvailabilityForm()
+        {
+            InitializeComponent();
+        }
+        private void ResultsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picLogo_Click(object sender, EventArgs e)
+        {
+            Utilities.toDashboard(sender, e, this);
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+        
+        //seperating this into a method allows it to work on all three dgvs without writing it over and over again
+        private void CheckForResults(DataTable dt, DataGridView dgv)
+        {
+            if (dt.Rows.Count > 0)
+            {
+                dgv.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("No data was found");
+            }
+        }
+       
+    }
+}
