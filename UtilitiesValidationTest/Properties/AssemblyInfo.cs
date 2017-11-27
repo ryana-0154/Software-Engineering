e0dc913de913b279dd160a9ef06ac45d96c3d1c6
ahmedi98 <ia403@student.anglia.ac.uk>
2017-11-27 21:54:57 +0000

    Unit testing (Phone Number, Description, Address & DOB

---
+++ UtilitiesValidationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UtilitiesValidationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UtilitiesValidationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67533e3e-18d7-428f-9c01-160d144bc02e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UtilitiesValidationTest/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using BasicGP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilitiesValidationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PhoneNumberValidationTest()
+        {
+            string txtPhoneNumber = "02075316006";
+
+            bool actual = Utilities.PhoneNumberValidation(txtPhoneNumber);
+
+            const bool expected = true;
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void DescriptionValidationTest()
+        {
+            string txtDescription = "Back ache";
+
+            bool actual = Utilities.DescriptionValidation(txtDescription);
+
+            const bool expected = true;
+            Assert.AreEqual(expected, actual);
+
+        }
+
+       
+        [TestMethod]
+        public void AddressValidationTest()
+        {
+            string txtAddress1 = "37 Castle Road";
+
+            bool actual = Utilities.AddressValidation(txtAddress1);
+
+            const bool expected = true;
+            Assert.AreEqual(expected, actual);
+
+        }
+        
+
+        [TestMethod]
+        public void DOBValidationTest()
+        {
+            DateTime dtpDOB = new DateTime(2003,05,28);
+
+            bool actual = Utilities.DOBValidation(dtpDOB);
+
+            const bool expected = true;
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
